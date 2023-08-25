c4d2e0373328f57f256d9577c9579ebde4887378
pangyuqiang <1498102047@qq.com>
2023-08-25 13:17:47 +0800

    chore(release): package markdown update

--- packages/axios/src/types.ts
+++ packages/axios/src/types.ts
@@ -20,14 +20,22 @@ export interface AxiosInterceptor {
 }
 
 export interface AxiosWrapper<T> {
+  /** Axios request 方法使用自定义配置发起 axios 请求 */
   request: (config: T) => Promise<any>
-  delete: (url: string, config?: T) => Promise<any>
+  /** HTTP GET 方法请求指定的资源。使用 GET 的请求应该只用于获取数据 */
   get: (url: string, params?: Dictionary, config?: T) => Promise<any>
-  head: (url: string, config?: T) => Promise<any>
-  options: (url: string, config?: T) => Promise<any>
-  patch: (url: string, data?: unknown, config?: T) => Promise<any>
+  /** HTTP POST 方法 发送数据给服务器。请求主体的类型由 Content-Type 首部指定 */
   post: (url: string, data?: unknown, config?: T) => Promise<any>
+  /** HTTP PUT 请求方法使用请求中的负载创建或者替换目标资源 */
   put: (url: string, data?: unknown, config?: T) => Promise<any>
+  /** HTTP DELETE 请求方法用于删除指定的资源 */
+  delete: (url: string, config?: T) => Promise<any>
+  /** 在 HTTP 协议中，请求方法 PATCH 用于对资源进行部分修改 */
+  patch: (url: string, data?: unknown, config?: T) => Promise<any>
+  /** HTTP HEAD 方法请求资源的标头信息，并且这些标头与 HTTP GET 方法请求时返回的一致 */
+  head: (url: string, config?: T) => Promise<any>
+  /** HTTP 的 OPTIONS 方法 用于获取目的资源所支持的通信选项 */
+  options: (url: string, config?: T) => Promise<any>
 }
 
 export interface CreateConfig extends Dictionary {
--- packages/axios/src/useAxios/index.ts
+++ packages/axios/src/useAxios/index.ts
@@ -10,36 +10,13 @@ import { CreateConfig } from '../types'
  *
  * @method
  * `axios.request(config)`
- *
- * Axios request 方法使用自定义配置发起 axios 请求
- *
  * `axios.get(url, params, config)`
- *
- * HTTP GET 方法请求指定的资源。使用 GET 的请求应该只用于获取数据
- *
  * `axios.post(url, data, config)`
- *
- * HTTP POST 方法 发送数据给服务器。请求主体的类型由 Content-Type 首部指定
- *
  * `axios.put(url, data, config)`
- *
- * HTTP PUT 请求方法使用请求中的负载创建或者替换目标资源
- *
  * `axios.delete(url, config)`
- *
- * HTTP DELETE 请求方法用于删除指定的资源
- *
  * `axios.patch(url, data, config)`
- *
- * 在 HTTP 协议中，请求方法 PATCH 用于对资源进行部分修改
- *
  * `axios.head(url, config)`
- *
- * HTTP HEAD 方法请求资源的标头信息，并且这些标头与 HTTP GET 方法请求时返回的一致
- *
  * `axios.options(url, config)`
- *
- * HTTP 的 OPTIONS 方法 用于获取目的资源所支持的通信选项
  *
  * @example
  * import { useAxios, useAxiosPack } from '@sdkset/axios'
